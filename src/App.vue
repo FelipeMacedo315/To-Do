808411956eb6795bb7de02f6202deec38e835a76
FelipeMacedo315 <felipemacedo315@gmail.com>
2022-10-20 12:16:56 -0300

    tarefas compartilhado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="search-form">
+      <input
+        v-bind:value="textTask"
+        v-on:change="captureTxtSearch"
+        type="search"
+      />
+      <button v-on:click="sendTxtCard">+</button>
+    </div>
+    <div id="container-all-cards" v-if="textTaskCard">
+      <Task v-bind:contentTask="textTaskCard" />
+    </div>
+    <div v-else></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Task from "./components/Task.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Task,
+  },
+  data() {
+    return {
+      textTask: "",
+      textTaskCard: [],
+    };
+  },
+  methods: {
+    captureTxtSearch(event) {
+      this.textTask = event.target.value;
+    },
+    sendTxtCard() {
+      this.textTask.length > 0 ? this.textTaskCard.push(this.textTask) : null;
+      this.textTask = "";
+    },
+  },
+};
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+}
 #app {
+  padding: 0%;
+  margin: 0%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+#container-all-cards {
+  width: 100%;
+}
+#search-form {
+  display: flex;
+  width: 30%;
+}
+input {
+  width: 100%;
+  padding: 5px;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="all-cards">
+    <section
+      v-bind:class="
+        taskStatusClass ? 'card-task-completed' : 'card-task-incompleted'
+      "
+      v-bind:key="index"
+      v-for="(task, index) in contentTask"
+      @dblclick="changeStatusTask"
+    >
+      <div class="close-icon">
+        <img
+          v-on:click="deleteTask(index)"
+          src="../assets/icons8-close-64.png"
+          alt="close-icon"
+        />
+      </div>
+      <p>{{ task }} indice {{ index }}</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["contentTask"],
+  data() {
+    return {
+      taskStatusClass: false,
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.contentTask.splice(index, 1);
+    },
+    changeStatusTask() {
+      this.taskStatusClass = !this.taskStatusClass;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#all-cards {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 25% 25% 25%;
+  grid-column-gap: 10%;
+  grid-row-gap: 15%;
+}
+.card-task-incompleted {
+  border-radius: 10px;
+  background-color: brown;
+  color: white;
+  width: 100%;
+  padding: 20px;
+  user-select: none;
+}
+.card-task-completed {
+  border-radius: 10px;
+  background-color: greenyellow;
+  color: white;
+  width: 100%;
+  padding: 20px;
+  user-select: none;
+}
+.close-icon {
+  display: flex;
+  justify-content: flex-end;
+}
+.close-icon img {
+  height: 15px;
+}
+</style>
